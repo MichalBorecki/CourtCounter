d20418ccd473edebd93e214b5a00d30c625e44c8
Michał Borecki <33671573+gitmbhub@users.noreply.github.com>
2018-03-03 23:11:04 +0100

    com04

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.android.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -13,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
     int scoreTeamB = 0;
     int scoreTeamAbackup;
     int scoreTeamBbackup;
-
     private String SCORE_TEAM_A = "SCORE_TEAM_A";
     private String SCORE_TEAM_B = "SCORE_TEAM_B";
     private String SCORE_TEAM_A_BACKUP = "SCORE_TEAM_A_BACKUP";
@@ -31,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
-
         super.onSaveInstanceState(outState);
         outState.putInt(SCORE_TEAM_A, scoreTeamA);
         outState.putInt(SCORE_TEAM_B, scoreTeamB);
@@ -43,49 +41,41 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onRestoreInstanceState(Bundle savedInstanceState) {
-
         super.onRestoreInstanceState(savedInstanceState);
-
-        // Restore UI state from the savedInstanceState.
-        // This bundle has also been passed to onCreate.
-
-
         scoreTeamA = savedInstanceState.getInt(SCORE_TEAM_A);
-        scoreTeamB = savedInstanceState.getInt (SCORE_TEAM_B);
+        scoreTeamB = savedInstanceState.getInt(SCORE_TEAM_B);
         scoreTeamAbackup = savedInstanceState.getInt(SCORE_TEAM_A_BACKUP);
-        scoreTeamBbackup = savedInstanceState.getInt (SCORE_TEAM_B_BACKUP);
+        scoreTeamBbackup = savedInstanceState.getInt(SCORE_TEAM_B_BACKUP);
         // displayForTeamA(scoreTeamAbackup);
-        // displayForTeamB(scoreTeamBbackup);
         displayForTeamA(scoreTeamA);
+        // displayForTeamB(scoreTeamBbackup);
         displayForTeamB(scoreTeamB);
-
     }
 
     /**
      * Buttons for Team A
      */
-
-    public void add5forTeamA (View view) {
+    public void add5forTeamA(View view) {
         scoreTeamA = scoreTeamA + 5;
         displayForTeamA(scoreTeamA);
     }
 
-    public void add4forTeamA (View view) {
+    public void add4forTeamA(View view) {
         scoreTeamA = scoreTeamA + 4;
         displayForTeamA(scoreTeamA);
     }
 
-    public void add3forTeamA (View view) {
+    public void add3forTeamA(View view) {
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
     }
 
-    public void add2forTeamA (View view) {
+    public void add2forTeamA(View view) {
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
 
-    public void add1forTeamA (View view) {
+    public void add1forTeamA(View view) {
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
     }
@@ -100,18 +90,17 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Buttons for Team B
      */
-
-    public void add5forTeamB (View view) {
+    public void add5forTeamB(View view) {
         scoreTeamB = scoreTeamB + 5;
         displayForTeamB(scoreTeamB);
     }
 
-    public void add4forTeamB (View view) {
+    public void add4forTeamB(View view) {
         scoreTeamB = scoreTeamB + 4;
         displayForTeamB(scoreTeamB);
     }
 
-    public void add3forTeamB (View view) {
+    public void add3forTeamB(View view) {
         scoreTeamB = scoreTeamB + 3;
         displayForTeamB(scoreTeamB);
     }
@@ -121,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamB(scoreTeamB);
     }
 
-    public void add1forTeamB (View view) {
+    public void add1forTeamB(View view) {
         scoreTeamB = scoreTeamB + 1;
         displayForTeamB(scoreTeamB);
     }
@@ -132,11 +121,11 @@ public class MainActivity extends AppCompatActivity {
     public void displayForTeamB(int score) {
         scoreViewB.setText(String.valueOf(score));
     }
+
     /**
      * Reset the score
      */
-
-    public void reset (View view) {
+    public void reset(View view) {
         scoreTeamAbackup = scoreTeamA;
         scoreTeamA = 0;
         scoreTeamBbackup = scoreTeamB;
@@ -148,12 +137,10 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Bring back last scores
      */
-
-    public void undo (View view) {
+    public void undo(View view) {
         scoreTeamA = scoreTeamAbackup;
         scoreTeamB = scoreTeamBbackup;
         displayForTeamA(scoreTeamA);
         displayForTeamB(scoreTeamB);
     }
-
 }
